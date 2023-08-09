a761b782d1aed33780eb30316f38e2f2528b148f
kevin wang <wqw@ankexun.com>
2023-08-09 15:53:41 +0800

    2023-08-09 wqw init:新建项目第一天

---
+++ internal/handler/login/userInfoHandler.go
@@ -0,0 +1,28 @@
+package login
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"okr/internal/logic/login"
+	"okr/internal/svc"
+	"okr/internal/types"
+)
+
+func UserInfoHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserInfoReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := login.NewUserInfoLogic(r.Context(), svcCtx)
+		resp, err := l.UserInfo(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
